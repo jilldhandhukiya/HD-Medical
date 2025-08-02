893d1151105afb3e484e937ca6cfbc90e98b7ec5
unknown <rushabhrathod018@gmail.com>
2025-08-03 00:58:07 +0530

    Home Page Done!

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
       <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet" />
       </head>
-      <body className={`${geist.className} m-0 p-0 font-sans bg-white`}>
+      <body className={`${geist.className} font-sans bg-white overflow-x-hidden`}>
             {children}
       </body>
     </html>
