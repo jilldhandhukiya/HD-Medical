da5490fe4713191bfa85c36f1907854b9fcc40b4
Jill Dhandhukiya <jilldhandhukiya2005@gmail.com>
2025-08-05 00:34:06 +0530

    added social medical preview support

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,36 @@
 import { Geist } from "next/font/google";
-import "./globals.css";
+import "./globals.css"; 
 
 const geist = Geist({
   subsets: ["latin"],
 });
 
 export const metadata = {
-  title: "HD Medical",
-  description: "Compassionate care, exceptional results",
+  title: 'HD Medical Group',
+  description: 'AI‑powered cardiovascular diagnostics and detection at the point of care.',
+  openGraph: {
+    title: 'HD Medical Group',
+    description: 'Global leader in AI-enabled cardiovascular health innovation.',
+    url: 'https://hdmedicalgroup.com',
+    siteName: 'HD Medical Group',
+    images: [
+      {
+        url: 'https://hdmedicalgroup.com/images/logo.png',
+        width: 1200,
+        height: 630,
+        alt: 'HD Medical Group image',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'HD Medical Group',
+    description: 'Next-gen cardiovascular diagnostics & AI-powered health tech.',
+    images: ['https://hdmedicalgroup.com/images/logo.png'],
+  },
 };
+
 
 export default function RootLayout({ children }) {
   return (
